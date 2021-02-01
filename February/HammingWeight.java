2270ce350ee596031062445c10fef111772da44e
Shahir Chowdhury <ry6262@gmail.com>
2021-02-01 15:47:02 -0500

    Feb day 1

---
+++ February/HammingWeight.java
@@ -0,0 +1,18 @@
+/**
+ * Feb 2021 Day 1
+ * 
+ * Just bit shift it to the right while tracking the right most bit
+ * Or use Integer.countBits
+ */
+
+public class HammingWeight {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count = 0;
+        for (int i = 0; i < 32; i++) {
+            count += n & 1;
+            n = n >> 1;
+        }
+        return count;
+    }
+}
