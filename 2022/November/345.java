14d5d0d1cbd57053617202e6b4c68a91962b0c0e
Shahir <shahirchowdhury9800@gmail.com>
2022-11-04 19:57:51 -0400

    mr data scientisttttttt

---
+++ 2022/November/345.java
@@ -0,0 +1,51 @@
+/*
+ * Reverse Vowels of a String
+ *
+ * 
+ */
+
+class Solution {
+    public String reverseVowels(String s) {
+        char[] c = s.toCharArray();
+        int[] indices = new int[c.length];
+        for (int i = 0; i < c.length; i++) {
+            if (isVowel(c[i])) {
+                indices[i] = i;
+            }
+            else {
+                indices[i] = -1;
+            }
+        }
+
+        int right = c.length - 1;
+        int left = 0;
+
+        while (left < right) {
+            if (indices[left] == -1) {
+                left++;
+            }
+            else if (indices[right] == -1) {
+                right--;
+            }
+            else {
+                char temp = c[left];
+                c[left] = c[right];
+                c[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        return new String(c);
+    }
+
+    private boolean isVowel(char c) {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            return true;
+        }
+        if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+            return true;
+        }
+        return false;
+    }
+}
