4027f206cee434df86aea916d1e7d6b159720a1e
Shahir <shahirchowdhury9800@gmail.com>
2023-05-15 12:28:20 -0400

    mayyyyyyyy

---
+++ 2023/May/203.java
@@ -0,0 +1,37 @@
+/*
+ * Remove Linked List Elements
+ *
+ * Top 100% (0ms)
+ * 
+ * Time Complexity: O(n)
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+        ListNode curr = head;
+
+        while (curr != null && curr.next != null) {
+            if (curr.next.val == val) {
+                curr.next = curr.next.next;
+            }
+            else {
+                curr = curr.next;
+            }
+        }
+
+        return head;
+    }
+}
